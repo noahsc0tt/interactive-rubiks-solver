4f6cb355bfee4441cca7ab48d23f94498601d5c9
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-06-26 18:05:50 +0100

    form navigator class

---
+++ Rubik's Cube Solver/Forms/FormNavigator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Rubiks_Cube_Solver
+{
+    internal static class FormNavigator
+    {
+        internal static void Navigate<T> (Form currentForm) where T : Form
+        {
+            Form nextForm = (Form) Activator.CreateInstance(typeof(T));
+            nextForm.Show();
+            currentForm.Hide();
+        }
+    }
+}
