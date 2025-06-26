ff2f511aa0655225e4d8ec1af7e2888003f01501
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-06-26 22:39:58 +0100

    moving FormNavigator to Util

---
+++ Rubik's Cube Solver/Util/FormNavigator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Rubiks_Cube_Solver
+{
+    internal static class FormNavigator
+    {
+        internal static void Navigate<T> (Form currentForm) where T : Form
+        {
+            Form nextForm = (Form) Activator.CreateInstance(typeof(T));
+            nextForm.Show();
+            currentForm.Hide();
+        }
+        
+        internal static void Navigate<T> (Form currentForm, params object[] args) where T : Form
+        {
+            Form nextForm = (Form) Activator.CreateInstance(typeof(T), args);
+            nextForm.Show();
+            currentForm.Hide();
+        }
+    }
+}
