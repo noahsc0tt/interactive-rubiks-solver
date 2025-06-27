4f61a825b182fa232c65b59c3ba9c644709105d9
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-06-27 19:59:13 +0100

    actually committing the change in CubeNetLocation of using PieceLocation validation methods

--- Rubik's Cube Solver/Solver/CellToPieceLocation.cs
+++ Rubik's Cube Solver/Solver/CellToPieceLocation.cs
@@ -16,20 +16,15 @@ namespace Rubiks_Cube_Solver
         public CubeNetLocation((int x, int y) location, Color face) : this(location.x, location.y, face) { }
         public CubeNetLocation(int x, int y, Color face)
         {
-            ValidateArg(x, nameof(x));
-            ValidateArg(y, nameof(y));
+            PieceLocation.ValidateCoord(x, nameof(x));
+            PieceLocation.ValidateCoord(y, nameof(y));
             X = x;
             Y = y;
             Face = face;
         }
-        private static bool IsCoordValid(int coord) => coord >= 0 && coord <= 2;
-
-        private static void ValidateArg(int coord, string paramName)
-        {
-            if (!IsCoordValid(coord))
-                throw new ArgumentOutOfRangeException(paramName, "Coordinate must be in the range 0-2");
-        }
+        
         public (int X, int Y, Color Face) GetLocation() => (X, Y, Face);
     }
 
+    
 }
