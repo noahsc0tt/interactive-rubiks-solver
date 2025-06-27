e2a0290a549f36baf15e6d3d5983ac8ecb16afaf
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-06-27 19:36:27 +0100

    actually adding CubeNetLocation to git

---
+++ Rubik's Cube Solver/Solver/CellToPieceLocation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubiks_Cube_Solver
+{
+    internal class CubeNetLocation
+    {
+        public readonly int X;
+        public readonly int Y;
+        public readonly Color Face;
+
+        public CubeNetLocation((int x, int y) location, Color face) : this(location.x, location.y, face) { }
+        public CubeNetLocation(int x, int y, Color face)
+        {
+            ValidateArg(x, nameof(x));
+            ValidateArg(y, nameof(y));
+            X = x;
+            Y = y;
+            Face = face;
+        }
+        private static bool IsCoordValid(int coord) => coord >= 0 && coord <= 2;
+
+        private static void ValidateArg(int coord, string paramName)
+        {
+            if (!IsCoordValid(coord))
+                throw new ArgumentOutOfRangeException(paramName, "Coordinate must be in the range 0-2");
+        }
+        public (int X, int Y, Color Face) GetLocation() => (X, Y, Face);
+    }
+
+}
