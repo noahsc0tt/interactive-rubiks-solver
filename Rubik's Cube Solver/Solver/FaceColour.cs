76ac0bcff2dd466f2ff8f2bdf3ff8a5133f08536
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-06-29 02:56:48 +0100

    added FaceColour enum with ToColor extension method

---
+++ Rubik's Cube Solver/Solver/FaceColour.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubiks_Cube_Solver
+{
+    internal enum FaceColour 
+    {
+        White,
+        Yellow,
+        Green,
+        Blue,
+        Red,
+        Orange
+    }
+
+    internal static class FaceColourToColor
+    {
+        public static Color ToColor(this FaceColour colour) => colour switch
+        {
+            FaceColour.White => Color.White,
+            FaceColour.Yellow => Color.Yellow,
+            FaceColour.Green => Color.Green,
+            FaceColour.Blue => Color.Blue,
+            FaceColour.Red => Color.Red,
+            FaceColour.Orange => Color.Orange
+        };
+    }
+}
