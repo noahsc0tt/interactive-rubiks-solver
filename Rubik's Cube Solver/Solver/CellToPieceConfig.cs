3252ed308078c87e9a2a23a19dcec3e9f8ad3a45
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-06-30 00:16:25 +0100

    2d -> 3d dicts in CellToPieceConfig populated

--- Rubik's Cube Solver/Solver/CellToPieceConfig.cs
+++ Rubik's Cube Solver/Solver/CellToPieceConfig.cs
@@ -1,5 +1,5 @@
-﻿using System.Linq;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
+using System.Linq;
 
 namespace Rubiks_Cube_Solver
 {
@@ -9,7 +9,7 @@ namespace Rubiks_Cube_Solver
     internal static class CellToPieceConfig
     {
         // dont need to be members, group them somehow (directly in switch statement?)
-        private static readonly Dict whiteDict = CreateColourDict
+        private static readonly Dict whiteDict = CreateFaceDict
             ([
                 ((0,2,2), PieceOrientation.Corner),
                 ((1,2,2), PieceOrientation.Good),
@@ -20,63 +20,63 @@ namespace Rubiks_Cube_Solver
                 ((1,2,0), PieceOrientation.Good),
                 ((2,2,0), PieceOrientation.Corner),
             ]);
-        private static readonly Dict yellowDict = CreateColourDict
+        private static readonly Dict yellowDict = CreateFaceDict
             ([
-                ((0,2,2), PieceOrientation.Corner),
-                ((1,2,2), PieceOrientation.Good),
-                ((2,2,2), PieceOrientation.Corner),
-                ((0,2,1), PieceOrientation.Good),
-                ((2,2,1), PieceOrientation.Good),
-                ((0,2,0), PieceOrientation.Corner),
-                ((1,2,0), PieceOrientation.Good),
-                ((2,2,0), PieceOrientation.Corner),
+                ((0,0,0), PieceOrientation.Corner),
+                ((1,0,0), PieceOrientation.Good),
+                ((2,0,0), PieceOrientation.Corner),
+                ((0,0,1), PieceOrientation.Good),
+                ((2,0,1), PieceOrientation.Good),
+                ((0,0,2), PieceOrientation.Corner),
+                ((1,0,2), PieceOrientation.Good),
+                ((2,0,2), PieceOrientation.Corner),
             ]);
-        private static readonly Dict greenDict = CreateColourDict
+        private static readonly Dict greenDict = CreateFaceDict
             ([
-                ((0,2,2), PieceOrientation.Corner),
-                ((1,2,2), PieceOrientation.Good),
-                ((2,2,2), PieceOrientation.Corner),
-                ((0,2,1), PieceOrientation.Good),
-                ((2,2,1), PieceOrientation.Good),
                 ((0,2,0), PieceOrientation.Corner),
-                ((1,2,0), PieceOrientation.Good),
+                ((1,2,0), PieceOrientation.Bad),
                 ((2,2,0), PieceOrientation.Corner),
+                ((0,1,0), PieceOrientation.Good),
+                ((2,1,0), PieceOrientation.Good),
+                ((0,0,0), PieceOrientation.Corner),
+                ((1,0,0), PieceOrientation.Bad),
+                ((2,0,0), PieceOrientation.Corner),
             ]);
-        private static readonly Dict blueDict = CreateColourDict
+        private static readonly Dict blueDict = CreateFaceDict
             ([
-                ((0,2,2), PieceOrientation.Corner),
-                ((1,2,2), PieceOrientation.Good),
                 ((2,2,2), PieceOrientation.Corner),
-                ((0,2,1), PieceOrientation.Good),
-                ((2,2,1), PieceOrientation.Good),
-                ((0,2,0), PieceOrientation.Corner),
-                ((1,2,0), PieceOrientation.Good),
-                ((2,2,0), PieceOrientation.Corner),
+                ((1,2,2), PieceOrientation.Bad),
+                ((0,2,2), PieceOrientation.Corner),
+                ((2,1,2), PieceOrientation.Good),
+                ((0,1,2), PieceOrientation.Good),
+                ((2,0,2), PieceOrientation.Corner),
+                ((1,0,2), PieceOrientation.Bad),
+                ((0,0,2), PieceOrientation.Corner),
             ]);
-        private static readonly Dict redDict = CreateColourDict
+        private static readonly Dict redDict = CreateFaceDict
             ([
-                ((0,2,2), PieceOrientation.Corner),
-                ((1,2,2), PieceOrientation.Good),
-                ((2,2,2), PieceOrientation.Corner),
-                ((0,2,1), PieceOrientation.Good),
-                ((2,2,1), PieceOrientation.Good),
-                ((0,2,0), PieceOrientation.Corner),
-                ((1,2,0), PieceOrientation.Good),
                 ((2,2,0), PieceOrientation.Corner),
+                ((2,2,1), PieceOrientation.Bad),
+                ((2,2,2), PieceOrientation.Corner),
+                ((2,1,0), PieceOrientation.Bad),
+                ((2,1,2), PieceOrientation.Bad),
+                ((2,0,0), PieceOrientation.Corner),
+                ((2,0,1), PieceOrientation.Bad),
+                ((2,0,2), PieceOrientation.Corner),
             ]);
-        private static readonly Dict orangeDict = CreateColourDict
+        private static readonly Dict orangeDict = CreateFaceDict
             ([
                 ((0,2,2), PieceOrientation.Corner),
-                ((1,2,2), PieceOrientation.Good),
-                ((2,2,2), PieceOrientation.Corner),
-                ((0,2,1), PieceOrientation.Good),
-                ((2,2,1), PieceOrientation.Good),
+                ((0,2,1), PieceOrientation.Bad),
                 ((0,2,0), PieceOrientation.Corner),
-                ((1,2,0), PieceOrientation.Good),
-                ((2,2,0), PieceOrientation.Corner),
+                ((0,1,2), PieceOrientation.Bad),
+                ((0,1,0), PieceOrientation.Bad),
+                ((0,0,2), PieceOrientation.Corner),
+                ((0,0,1), PieceOrientation.Bad),
+                ((0,0,0), PieceOrientation.Corner),
             ]);
 
-        private static Dict GetColourDict(FaceColour colour) => colour switch
+        private static Dict GetFaceDict(FaceColour colour) => colour switch
         {
             FaceColour.White => whiteDict,
             FaceColour.Yellow => yellowDict,
@@ -86,7 +86,7 @@ namespace Rubiks_Cube_Solver
             FaceColour.Orange => orangeDict
         };
 
-        private static Dict CreateColourDict(((int, int, int) location, PieceOrientation orientation)[] pieceConfigs)
+        private static Dict CreateFaceDict(((int, int, int) location, PieceOrientation orientation)[] pieceConfigs)
             => ImmutableDictionary.CreateRange
             (
                 new[] { (0, 0), (1, 0), (2, 0), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2) } // the cell coordinates on each face
@@ -98,6 +98,6 @@ namespace Rubiks_Cube_Solver
 
 
         public static PieceConfig GetPieceConfig(CubeNetCellLocation cellLocation) =>
-            GetColourDict(cellLocation.Face)[(cellLocation.X, cellLocation.Y)];
+            GetFaceDict(cellLocation.Face)[(cellLocation.X, cellLocation.Y)];
     }
 }
