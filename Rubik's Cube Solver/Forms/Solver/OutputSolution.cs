1933237a239452ef022dacc05b7fb1148f0b251a
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-07-02 17:56:05 +0100

    removed redundant syntax in RotatePieceLocation

--- Rubik's Cube Solver/Forms/Solver/OutputSolution.cs
+++ Rubik's Cube Solver/Forms/Solver/OutputSolution.cs
@@ -25,7 +25,6 @@ namespace Rubiks_Cube_Solver
 
             //checking if the piece is an edge
             if (x == 1 || y == 1 || z == 1)
-            {
                 (x, z) = y switch
                 {
                     0 => z switch
@@ -35,9 +34,8 @@ namespace Rubiks_Cube_Solver
                         2 => (0, 1)
                     },
                     1 => (z == 0) ? (2, x) : (0, x),
-                    2 => (x == 1) ? (z == 2 ? 0 : 2, 1) : (x, z) = (z, x)
+                    2 => (x == 1) ? (z == 2 ? 0 : 2, 1) : (z, x)
                 };
-            }
             else //if the piece is a corner
                 (x, z) = (z == 2 ? 0 : 2, x);
 
