f67f28dddbdce0097a4e02f62567a90260f825f7
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-07-02 21:51:00 +0100

    frame for SolvePieceClass

---
+++ Rubik's Cube Solver/Solver/SolvePiece.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Rubiks_Cube_Solver
+{
+    using Dict = ImmutableDictionary<PieceConfig, PieceSolution>;
+    using Entry = KeyValuePair<PieceConfig, PieceSolution>;
+    internal static class SolvePiece
+    {
+        private static readonly Dict yellowEdgesDict;
+        private static readonly Dict yellowCornersDict;
+        private static readonly Dict middleLayerEdgesDict;
+
+        private static Entry CreateCornerEntry((int, int, int) location, string sequence, string explanation) =>
+            CreateEntry(location, PieceOrientation.Corner, sequence, explanation);
+
+        private static Entry CreateEntry((int, int, int) location, PieceOrientation orientation, string sequence, string explanation) =>
+            new Entry(new PieceConfig(location, orientation), new PieceSolution(sequence, explanation));
+
+        public static Dict GetStageDict(Stage stage) =>
+            stage.Step switch
+            {
+                0 => yellowEdgesDict,
+                1 => yellowCornersDict,
+                2 => middleLayerEdgesDict,
+                _ => throw new InvalidOperationException("Last layer stages do not have PieceSolutions")
+            };
+
+        public static PieceSolution GetSolution(Stage stage, PieceConfig piece) =>
+            GetStageDict(stage)[piece];
+    }
+}
