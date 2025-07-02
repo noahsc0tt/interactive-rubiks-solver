818c89671b4bb5dcff73c573ee7cafccc987b74c
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-07-02 18:33:33 +0100

    PieceSolution pseudo-record class to store sequence and explanation

---
+++ Rubik's Cube Solver/Solver/PieceSolution.cs
@@ -0,0 +1,11 @@
+﻿namespace Rubiks_Cube_Solver
+{
+    internal class PieceSolution
+    {
+        public string Sequence { get; private set; }
+        public string Explanation { get; private set; }
+
+        public PieceSolution(string sequence, string explanation) =>
+            (Sequence, Explanation) = (sequence, explanation);
+    }
+}
