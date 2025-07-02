0e36449e6391865f8dd6614898fb90c1431bd650
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-07-02 21:24:43 +0100

    updating orientation properly in RotatePiece

--- Rubik's Cube Solver/Solver/RotatePiece.cs
+++ Rubik's Cube Solver/Solver/RotatePiece.cs
@@ -9,6 +9,8 @@
 
             //checking if the piece is an edge
             if (x == 1 || y == 1 || z == 1)
+            {
+                if (y == 1) orientation = orientation.Flip();
                 (x, z) = y switch
                 {
                     0 => z switch
@@ -20,6 +22,7 @@
                     1 => (z == 0) ? (2, x) : (0, x),
                     2 => (x == 1) ? (z == 2 ? 0 : 2, 1) : (z, x)
                 };
+            }
             else //if the piece is a corner
                 (x, z) = (z == 2 ? 0 : 2, x);
 
