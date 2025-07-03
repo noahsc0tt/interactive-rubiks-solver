bdee4359f9719a989e6944bceed73eeef716e304
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-07-03 16:00:19 +0100

    moving explanations to separate arrays for maintainability

--- Rubik's Cube Solver/Solver/SolvePiece.cs
+++ Rubik's Cube Solver/Solver/SolvePiece.cs
@@ -8,63 +8,87 @@ namespace Rubiks_Cube_Solver
     using Entry = KeyValuePair<PieceConfig, PieceSolution>;
     internal static class SolvePiece
     {
+
+        private static readonly string[] yellowEdgesExplanations =
+            [
+                "Bring the edge to the top layer. Position it above the correct slot.",
+                "Bring the edge next to the correct slot.",
+                "Insert the edge into the correct slot.",
+                "Position the edge above the correct slot.",
+                "Bring the edge to the top layer. Move it across. Bring it next to the correct slot.",
+                "Move the edge across. Bring it next to the correct slot."
+            ];
+
+        private static readonly string[] yellowCornersExplanations =
+            [
+                "Bring the corner to the top layer. Position it above the correct slot.",
+                "Position the corner above the correct slot."
+            ];
+
+        private static readonly string[] middleLayerEdgesExplanations =
+            [
+                "Take the edge out of the incorrect slot. Re-insert the corner. Match the edge up with the corresponding corner.",
+                "Match the edge up with the corresponding corner.",
+                "Take the pair out of their slot. Match the edge up with the corner."
+            ];
+
         private static readonly Dict yellowEdgesDict = ImmutableDictionary.CreateRange
             ([
-                CreateEntry((0,0,1), PieceOrientation.Good, "L2, U', F2", "Bring the edge to the top layer. Position it above the correct slot."),
-                CreateEntry((0,0,1), PieceOrientation.Bad, "L', F'", "Bring the edge next to the correct slot."),
-                CreateEntry((0,1,0), PieceOrientation.Good, "F'", "Insert the edge into the correct slot."),
-                CreateEntry((0,1,0), PieceOrientation.Bad, "L', U', L, F2", "Bring the edge to the top layer. Position it above the correct slot."),
-                CreateEntry((0,1,2), PieceOrientation.Good, "L, U', L', F2", "Bring the edge to the top layer. Position it above the correct slot."),
-                CreateEntry((0,1,2), PieceOrientation.Bad, "L2, F', L2", "Bring the edge next to the correct slot."),
-                CreateEntry((0,2,1), PieceOrientation.Good, "U', F2", "Position the edge above the correct slot."),
-                CreateEntry((0,2,1), PieceOrientation.Bad, "L, F', L'", "Bring the edge next to the correct slot."),
+                CreateEntry((0,0,1), PieceOrientation.Good, "L2, U', F2", yellowEdgesExplanations[0]),
+                CreateEntry((0,0,1), PieceOrientation.Bad, "L', F'", yellowEdgesExplanations[1]),
+                CreateEntry((0,1,0), PieceOrientation.Good, "F'", yellowEdgesExplanations[2]),
+                CreateEntry((0,1,0), PieceOrientation.Bad, "L', U', L, F2", yellowEdgesExplanations[0]),
+                CreateEntry((0,1,2), PieceOrientation.Good, "L, U', L', F2", yellowEdgesExplanations[0]),
+                CreateEntry((0,1,2), PieceOrientation.Bad, "L2, F', L2", yellowEdgesExplanations[1]),
+                CreateEntry((0,2,1), PieceOrientation.Good, "U', F2", yellowEdgesExplanations[3]),
+                CreateEntry((0,2,1), PieceOrientation.Bad, "L, F', L'", yellowEdgesExplanations[1]),
                 CreateEntry((1,0,0), PieceOrientation.Good, "Already Solved", "Already Solved."),
-                CreateEntry((1,0,0), PieceOrientation.Bad, "F2, U', R', F, R", "Bring the edge to the top layer. Move it across. Bring it next to the correct slot."),
-                CreateEntry((1,0,2), PieceOrientation.Good, "B2, U2, F2", "Bring the edge to the top layer. Position it above the correct slot."),
-                CreateEntry((1,0,2), PieceOrientation.Bad, "B2, U, R', F, R", "Bring the edge to the top layer. Move it across. Bring it next to the correct slot."),
-                CreateEntry((1,2,0), PieceOrientation.Good, "F2", "Insert the edge into the correct slot."),
-                CreateEntry((1,2,0), PieceOrientation.Bad, "U', R', F, R", "Move the edge across. Bring it next to the correct slot."),
-                CreateEntry((1,2,2), PieceOrientation.Good, "U2, F2", "Position the edge above the correct slot."),
-                CreateEntry((1,2,2), PieceOrientation.Bad, "U, R', F, R", "Move the edge across. Bring it next to the correct slot."),
-                CreateEntry((2,0,1), PieceOrientation.Good, "R2, U, F2", "Bring the edge to the top layer. Position it above the correct slot."),
-                CreateEntry((2,0,1), PieceOrientation.Bad, "R, U, F2", "Bring the edge next to the correct slot."),
-                CreateEntry((2,1,0), PieceOrientation.Good, "R, U, R', F2", "Bring the edge to the top layer. Position it above the correct slot."),
-                CreateEntry((2,1,0), PieceOrientation.Bad, "F", "Insert the edge into the correct slot."),
-                CreateEntry((2,1,2), PieceOrientation.Good, "R', U, R, F2", "Bring the edge to the top layer. Position it above the correct slot."),
-                CreateEntry((2,1,2), PieceOrientation.Bad, "R2, F, R2", "Insert the edge into the correct slot."),
-                CreateEntry((2,2,1), PieceOrientation.Good, "U, F2", "Position the edge above the correct slot."),
-                CreateEntry((2,2,1), PieceOrientation.Bad, "R', F, R", "Bring the edge next to the correct slot.")
+                CreateEntry((1,0,0), PieceOrientation.Bad, "F2, U', R', F, R", yellowEdgesExplanations[4]),
+                CreateEntry((1,0,2), PieceOrientation.Good, "B2, U2, F2", yellowEdgesExplanations[0]),
+                CreateEntry((1,0,2), PieceOrientation.Bad, "B2, U, R', F, R", yellowEdgesExplanations[4]),
+                CreateEntry((1,2,0), PieceOrientation.Good, "F2", yellowEdgesExplanations[2]),
+                CreateEntry((1,2,0), PieceOrientation.Bad, "U', R', F, R", yellowEdgesExplanations[5]),
+                CreateEntry((1,2,2), PieceOrientation.Good, "U2, F2", yellowEdgesExplanations[3]),
+                CreateEntry((1,2,2), PieceOrientation.Bad, "U, R', F, R", yellowEdgesExplanations[5]),
+                CreateEntry((2,0,1), PieceOrientation.Good, "R2, U, F2", yellowEdgesExplanations[0]),
+                CreateEntry((2,0,1), PieceOrientation.Bad, "R, U, F2", yellowEdgesExplanations[1]),
+                CreateEntry((2,1,0), PieceOrientation.Good, "R, U, R', F2", yellowEdgesExplanations[0]),
+                CreateEntry((2,1,0), PieceOrientation.Bad, "F", yellowEdgesExplanations[2]),
+                CreateEntry((2,1,2), PieceOrientation.Good, "R', U, R, F2", yellowEdgesExplanations[0]),
+                CreateEntry((2,1,2), PieceOrientation.Bad, "R2, F, R2", yellowEdgesExplanations[2]),
+                CreateEntry((2,2,1), PieceOrientation.Good, "U, F2", yellowEdgesExplanations[3]),
+                CreateEntry((2,2,1), PieceOrientation.Bad, "R', F, R", yellowEdgesExplanations[1])
             ]);
 
         private static readonly Dict yellowCornersDict = ImmutableDictionary.CreateRange
             ([
-                CreateCornerEntry((0,0,0), "L', U', L", "Bring the corner to the top layer. Position it above the correct slot."),
-                CreateCornerEntry((0,0,2), "L, U, L', U", "Bring the corner to the top layer. Position it above the correct slot."),
-                CreateCornerEntry((0,2,0), "U'", "Position the corner above the correct slot."),
-                CreateCornerEntry((0,2,2), "U2", "Position the corner above the correct slot."),
+                CreateCornerEntry((0,0,0), "L', U', L", yellowCornersExplanations[0]),
+                CreateCornerEntry((0,0,2), "L, U, L', U", yellowCornersExplanations[0]),
+                CreateCornerEntry((0,2,0), "U'", yellowCornersExplanations[1]),
+                CreateCornerEntry((0,2,2), "U2", yellowCornersExplanations[1]),
                 CreateCornerEntry((2,0,0), "", ""),
-                CreateCornerEntry((2,0,2), "R', U, R, U", "Bring the corner to the top layer. Position it above the correct slot."),
+                CreateCornerEntry((2,0,2), "R', U, R, U", yellowCornersExplanations[0]),
                 CreateCornerEntry((2,2,0), "", ""),
-                CreateCornerEntry((2,2,2), "U", "Position the corner above the correct slot.")
+                CreateCornerEntry((2,2,2), "U", yellowCornersExplanations[1])
             ]);
         private static readonly Dict middleLayerEdgesDict = ImmutableDictionary.CreateRange
             ([
-                CreateEntry((0,1,0), PieceOrientation.Good, "F, U', F', U', L', U, L, U2, R, U', R', U', F', U, F", "Take the edge out of the incorrect slot. Re-insert the corner. Match the edge up with the corresponding corner."),
-                CreateEntry((0,1,0), PieceOrientation.Bad, "F, U', F', U', L', U, L, U', F', U, F, U, R, U', R'", "Take the edge out of the incorrect slot. Re-insert the corner. Match the edge up with the corresponding corner."),
-                CreateEntry((0,1,2), PieceOrientation.Good, "B', U, B, U, L, U', L', U2, R, U', R', U', F', U, F", "Take the edge out of the incorrect slot. Re-insert the corner. Match the edge up with the corresponding corner."),
-                CreateEntry((0,1,2), PieceOrientation.Bad, "B', U, B, U, L, U', L', U', F', U, F, U, R, U', R'", "Take the edge out of the incorrect slot. Re-insert the corner. Match the edge up with the corresponding corner."),
-                CreateEntry((0,2,1), PieceOrientation.Good, "U, F', U, F, U, R, U', R'", "Match the edge up with the corresponding corner."),
-                CreateEntry((0,2,1), PieceOrientation.Bad, "R, U', R', U', F', U, F", "Match the edge up with the corresponding corner."),
-                CreateEntry((1,2,0), PieceOrientation.Good, "U2, F', U, F, U, R, U', R'", "Match the edge up with the corresponding corner."),
-                CreateEntry((1,2,0), PieceOrientation.Bad, "U, R, U', R', U', F', U, F", "Match the edge up with the corresponding corner."),
-                CreateEntry((1,2,2), PieceOrientation.Good, "F', U, F, U, R, U', R'", "Match the edge up with the corresponding corner."),
-                CreateEntry((1,2,2), PieceOrientation.Bad, "U', R, U', R', U', F', U, F", "Match the edge up with the corresponding corner."),
+                CreateEntry((0,1,0), PieceOrientation.Good, "F, U', F', U', L', U, L, U2, R, U', R', U', F', U, F", middleLayerEdgesExplanations[0]),
+                CreateEntry((0,1,0), PieceOrientation.Bad, "F, U', F', U', L', U, L, U', F', U, F, U, R, U', R'", middleLayerEdgesExplanations[0]),
+                CreateEntry((0,1,2), PieceOrientation.Good, "B', U, B, U, L, U', L', U2, R, U', R', U', F', U, F", middleLayerEdgesExplanations[0]),
+                CreateEntry((0,1,2), PieceOrientation.Bad, "B', U, B, U, L, U', L', U', F', U, F, U, R, U', R'", middleLayerEdgesExplanations[0]),
+                CreateEntry((0,2,1), PieceOrientation.Good, "U, F', U, F, U, R, U', R'", middleLayerEdgesExplanations[1]),
+                CreateEntry((0,2,1), PieceOrientation.Bad, "R, U', R', U', F', U, F", middleLayerEdgesExplanations[1]),
+                CreateEntry((1,2,0), PieceOrientation.Good, "U2, F', U, F, U, R, U', R'", middleLayerEdgesExplanations[1]),
+                CreateEntry((1,2,0), PieceOrientation.Bad, "U, R, U', R', U', F', U, F", middleLayerEdgesExplanations[1]),
+                CreateEntry((1,2,2), PieceOrientation.Good, "F', U, F, U, R, U', R'", middleLayerEdgesExplanations[1]),
+                CreateEntry((1,2,2), PieceOrientation.Bad, "U', R, U', R', U', F', U, F", middleLayerEdgesExplanations[1]),
                 CreateEntry((2,1,0), PieceOrientation.Good, "Already solved.", "Already solved."),
-                CreateEntry((2,1,0), PieceOrientation.Bad, "R, U', R', U, F', U2, F, U2, F', U, F", "Take the pair out of their slot. Match the edge up with the corner."),
-                CreateEntry((2,1,2), PieceOrientation.Good, "B, U', B', U', R', U, R, R, U', R', U', F', U, F", "Take the edge out of the incorrect slot. Re-insert the corner. Match the edge up with the corresponding corner."),
-                CreateEntry((2,1,2), PieceOrientation.Bad, "B, U', B', U', R', U, R, U, F', U, F, U, R, U', R'", "Take the edge out of the incorrect slot. Re-insert the corner. Match the edge up with the corresponding corner."),
-                CreateEntry((2,2,1), PieceOrientation.Good, "U', F', U, F, U, R, U', R'", "Match the edge up with the corresponding corner."),
-                CreateEntry((2,2,1), PieceOrientation.Bad, "U2, R, U', R', U', F', U, F", "Match the edge up with the corresponding corner."),
+                CreateEntry((2,1,0), PieceOrientation.Bad, "R, U', R', U, F', U2, F, U2, F', U, F", middleLayerEdgesExplanations[2]),
+                CreateEntry((2,1,2), PieceOrientation.Good, "B, U', B', U', R', U, R, R, U', R', U', F', U, F", middleLayerEdgesExplanations[0]),
+                CreateEntry((2,1,2), PieceOrientation.Bad, "B, U', B', U', R', U, R, U, F', U, F, U, R, U', R'", middleLayerEdgesExplanations[0]),
+                CreateEntry((2,2,1), PieceOrientation.Good, "U', F', U, F, U, R, U', R'", middleLayerEdgesExplanations[1]),
+                CreateEntry((2,2,1), PieceOrientation.Bad, "U2, R, U', R', U', F', U, F", middleLayerEdgesExplanations[1]),
             ]);
 
         private static Entry CreateCornerEntry((int, int, int) location, string sequence, string explanation) =>
@@ -74,9 +98,9 @@ namespace Rubiks_Cube_Solver
 
         private static Entry CreateEntry((int, int, int) location, PieceOrientation orientation, string sequence, string explanation)
         {
-            if (explanation.EndsWith("slot."))
-                explanation += " Insert it.";
-            else if (explanation.EndsWith("corner."))
+            if (explanation.EndsWith("slot.")) // entries in yellowEdgesDict
+                explanation += "Insert it."; 
+            else if (explanation.EndsWith("corner.")) // entries in middleLayerEdgesDict
                 explanation += " Insert the pair into the correct slot.";
 
             return new(new PieceConfig(location, orientation), new PieceSolution(sequence, explanation));
