c7e214f37b6b44a4b861d4e391fc1b1706a7e62b
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-07-05 18:58:26 +0100

    better scrambler ensuring three consecutive moves have to be on different faces

--- Rubik's Cube Solver/Util/Scrambler.cs
+++ Rubik's Cube Solver/Util/Scrambler.cs
@@ -6,25 +6,25 @@ namespace Rubiks_Cube_Solver
     {
         private static readonly string[] faces = { "U", "D", "F", "B", "L", "R" };
         private static readonly string[] suffixes = { "", "'", "2" };
-        private static readonly Random random = new Random();
+        private static readonly Random random = new();
         private const int length = 20;
 
         public static string GetScramble()
         {
             string scramble = "";
-            char lastFace = '\0';
+            (char, char) lastTwoFaces = ('\0', '\0');
             string nextMove;
 
             for (int i = 0; i < length; i++)
             {
-                do
-                {
+                do //making sure three consecutive moves are on different faces
                     nextMove = GetMove();
-                } while (nextMove[0] == lastFace); //making sure consecutive moves are on different faces
+                while (nextMove[0] == lastTwoFaces.Item1 || nextMove[0] == lastTwoFaces.Item2); 
 
-                if (i > 0) scramble += ", ";
+                if (i > 0)
+                    scramble += ", ";
                 scramble += nextMove;
-                lastFace = nextMove[0];
+                lastTwoFaces = (lastTwoFaces.Item2, nextMove[0]);
             }
 
             return scramble;
