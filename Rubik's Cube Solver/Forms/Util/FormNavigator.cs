dfbd7c987497c4cd514935bf50dd08274d91cbd2
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-07-15 00:56:45 +0100

    immutable formnavigator dictionary

--- Rubik's Cube Solver/Forms/Util/FormNavigator.cs
+++ Rubik's Cube Solver/Forms/Util/FormNavigator.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Windows.Forms;
 
 namespace Rubiks_Cube_Solver.Forms.Util
 {
+    using SingletonEntry = KeyValuePair<Type, Form>;
+
     internal static class FormNavigator
     {
-        private static readonly Dictionary<Type, Form> singletons = new()
-        {
-            { typeof(Menu), Forms.Menu.Instance },
-            { typeof(NotationExplanation), NotationExplanation.Instance }
-        };
+        private static readonly ImmutableDictionary<Type, Form> singletons = ImmutableDictionary.CreateRange
+            ([
+                new SingletonEntry(typeof(Menu), Forms.Menu.Instance),
+                new SingletonEntry(typeof(NotationExplanation), NotationExplanation.Instance)
+            ]);
 
         public static void Menu(Form currentForm) =>
             Navigate<Menu>(currentForm);
